7785c425b5f93b342f35223453ed94a9956c94be
zoucl@caimore.com <crmxmcmtxkjyxgs_81202>
2024-12-11 08:47:14 +0800

    fibocom remove

--- rubin-script.sh
+++ rubin-script.sh
@@ -4,4 +4,5 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 ./scripts/feeds update -a
+rm -rf feeds/packages/kernel/fibocom-qmi-wwan/
 ./scripts/feeds install -a
