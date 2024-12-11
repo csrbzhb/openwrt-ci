4df0cd4e635dce63d0dc9e709cf0f89367362281
zoucl@caimore.com <crmxmcmtxkjyxgs_81202>
2024-12-11 13:56:33 +0800

    fix fibocom driver

--- rubin-script.sh
+++ rubin-script.sh
@@ -4,5 +4,4 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 ./scripts/feeds update -a
-rm -rf feeds/packages/kernel/fibocom-qmi-wwan/
 ./scripts/feeds install -a
